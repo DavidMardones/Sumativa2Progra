6bfedc834f94971a52e97d3ead194f0ed7f9255d
David Mardones <david.mardones04@inacapmail.cl>
2023-05-02 16:46:45 -0400

    Agregar archivos de proyecto.

---
+++ Cliente/Program.cs
@@ -0,0 +1,46 @@
+﻿using Cliente.Comunicacion;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cliente
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int puerto = Convert.ToInt32(ConfigurationManager.AppSettings["puerto"]);
+            string servidor = ConfigurationManager.AppSettings["servidor"];
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Conectando a Servidor {0} en puerto {1}", servidor, puerto);
+            ClienteSocket clienteSocket = new ClienteSocket(servidor, puerto);
+            if (clienteSocket.Conectar())
+            {
+                Console.WriteLine("Conectado...");
+                string mensaje = clienteSocket.Leer();
+                Console.WriteLine("M: {0}", mensaje);
+                while (true)
+                {
+                    string nombre = Console.ReadLine().Trim();
+                    clienteSocket.Escribir(nombre);
+                    mensaje = clienteSocket.Leer();
+                    Console.WriteLine("M: {0}", mensaje);
+                    if (mensaje == "Chao")
+                    {
+                        clienteSocket.Desconectar();
+                        Console.WriteLine("Desconectado");
+                    }
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("Error de comunicacion");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Sumativa2Progra/Program.cs
@@ -0,0 +1,56 @@
+﻿using Sumativa2Progra.Comunicacion;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sumativa2Progra
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int puerto = Convert.ToInt32(ConfigurationManager.AppSettings["puerto"]);
+
+            Console.WriteLine("Iniciando servidor en puerto {0}", puerto);
+            ServerSocket servidor = new ServerSocket(puerto);
+
+            if (servidor.Iniciar())
+            {
+                Console.WriteLine("Servidor iniciado");
+                Console.WriteLine("Esperando Cliente...");
+                Socket socketCliente = servidor.ObtenerCliente();
+                Console.WriteLine("Cliente Conectado");
+                ClienteCom cliente = new ClienteCom(socketCliente);
+                cliente.Escribir("Bienvenido cliente ");
+                while (true)
+                {
+                    string respuesta = cliente.Leer();
+                    Console.WriteLine("El cliente envio: {0}", respuesta);
+                    {
+                        if (respuesta != "Chao")
+                        {
+                            string mensaje = Console.ReadLine();
+                            cliente.Escribir(mensaje);
+                            Console.WriteLine("Enviaste: " + mensaje);
+                        }
+                        else
+                        {
+                            cliente.Desconectar();
+                        }
+                    }
+
+
+                }
+            }
+
+            else
+            {
+                Console.WriteLine("ERROR, EL PUERTO ESTA {0} ESTA EN USO", puerto);
+            }
+        }
+    }
+}
